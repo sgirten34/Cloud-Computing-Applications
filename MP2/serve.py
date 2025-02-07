cf0268ca4c5cb9c32309f5ae4eec59588e10f759
Scott Girten <sgirten2@illinois.edu>
2025-02-06 22:16:32 -0500

    Formatting changes to initial code

--- MP2/serve.py
+++ MP2/serve.py
@@ -6,16 +6,13 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def serve():
-    # Stress the CPU to the specified usage
+    
     if request.method == 'POST':
-        # Create a subprocess to run the stress_cpu.py script
-        # It will stress to 100% utilization for approximately 1 minute
         subprocess.Popen(["python3", "stress_cpu.py"]) 
         hostname = socket.gethostname()
-        return f"push EC2 instance {socket.gethostbyname(hostname)} to maximum CPU utilization"
+        #return f"push EC2 instance {socket.gethostbyname(hostname)} to maximum CPU utilization"
 
-    # Return the IP address of the current EC2 instance
-    if request.method == 'GET':
+    elif request.method == 'GET':
         hostname = socket.gethostname()
         return socket.gethostbyname(hostname)
 
