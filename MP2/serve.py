bf348337143e8ad740aba808209ec3a3fa29ea4e
Scott Girten <sgirten2@illinois.edu>
2025-02-23 00:16:02 -0500

    Updated port on server

--- MP2/serve.py
+++ MP2/serve.py
@@ -10,10 +10,11 @@ def serve():
     if request.method == 'POST':
         subprocess.Popen(["python3", "stress_cpu.py"]) 
         hostname = socket.gethostname()
+        return f"EC2 instance {socket.gethostbyname(hostname)} towards maxi CPU utilization"
        
     elif request.method == 'GET':
         hostname = socket.gethostname()
         return socket.gethostbyname(hostname)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=8000)
+    app.run(host='0.0.0.0',port=5000)
--- MP2/submit.py
+++ MP2/submit.py
@@ -10,10 +10,10 @@ YOUR_SECRET = "Ovm8iiFsErTTQEiz" # <put your secret token from coursera>
 SECTION = "" # <put "1" to test section 1 in isolation; put "2" to test section 2 in isolation>
 
 # Section 1
-IP_ADDRESS1 = "18.208.152.40:8000" # <put your first EC2 instance's IP address:port>
-IP_ADDRESS2 = "52.91.26.84:8000" # <put your second instance's IP address:port>
+IP_ADDRESS1 = "3.12.123.86:5000" # <put your first EC2 instance's IP address:port>
+IP_ADDRESS2 = "18.117.102.99:5000" # <put your second instance's IP address:port>
 # Do not add "https://" to your loadbalancer address.
-YOUR_LOAD_BALANCER1 = "mp2LoadBalancerSection1-xxxx.us-east-1.elb.amazonaws.com" # <put your load_balancer address for section 1 (explicitly add :port if port is not 80)>
+YOUR_LOAD_BALANCER1 = "mp2-section1-alb-1766071293.us-east-2.elb.amazonaws.com" # <put your load_balancer address for section 1 (explicitly add :port if port is not 80)>
 # Section 2
 YOUR_LOAD_BALANCER2 = "mp2LoadBalancerSection2-xxxx.us-east-1.elb.amazonaws.com" # <put your load_balancer address for section 2 (explicitly add :port if port is not 80)>, 
 
