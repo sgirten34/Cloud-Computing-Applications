53efe08334a6423bee3876ba6b34cdd2c5b1a689
Scott Girten <sgirten2@illinois.edu>
2025-02-24 22:04:52 -0500

    updated submit file

--- MP2/submit.py
+++ MP2/submit.py
@@ -4,7 +4,7 @@ import json
 ''' Fill in the following information '''
 # General information
 YOUR_EMAIL = "sgirten2@illinois.edu" # <put your coursera account email>,
-YOUR_SECRET = "Ovm8iiFsErTTQEiz" # <put your secret token from coursera>
+YOUR_SECRET = "tE5EfDoThCcIEoKt" # <put your secret token from coursera>
 
 # <To get full credits, leave this blank. It will run both section 1 and section 2 tests>
 SECTION = "" # <put "1" to test section 1 in isolation; put "2" to test section 2 in isolation>
@@ -13,9 +13,9 @@ SECTION = "" # <put "1" to test section 1 in isolation; put "2" to test section 
 IP_ADDRESS1 = "3.12.123.86:5000" # <put your first EC2 instance's IP address:port>
 IP_ADDRESS2 = "18.117.102.99:5000" # <put your second instance's IP address:port>
 # Do not add "https://" to your loadbalancer address.
-YOUR_LOAD_BALANCER1 = "mp2-section1-alb-1766071293.us-east-2.elb.amazonaws.com" # <put your load_balancer address for section 1 (explicitly add :port if port is not 80)>
+YOUR_LOAD_BALANCER1 = "mp2-1-alb-344811694.us-east-2.elb.amazonaws.com" # <put your load_balancer address for section 1 (explicitly add :port if port is not 80)>
 # Section 2
-YOUR_LOAD_BALANCER2 = "mp2LoadBalancerSection2-xxxx.us-east-1.elb.amazonaws.com" # <put your load_balancer address for section 2 (explicitly add :port if port is not 80)>, 
+YOUR_LOAD_BALANCER2 = "mp2-asg-alb-1090954640.us-east-2.elb.amazonaws.com" # <put your load_balancer address for section 2 (explicitly add :port if port is not 80)>, 
 
 ''' Don't change the following '''
 url = "https://ekwygde36j.execute-api.us-east-1.amazonaws.com/alpha/execution"
